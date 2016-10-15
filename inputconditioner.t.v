e8ad862fdaba777abc1392640331b2877b8612ae
lisahachmann <lisa.hachmann@gmail.com>
2016-10-15 12:10:14 -0400

    Not quite....but started

--- inputconditioner.t.v
+++ inputconditioner.t.v
@@ -1,6 +1,7 @@
 //------------------------------------------------------------------------
 // Input Conditioner test bench
 //------------------------------------------------------------------------
+`include "inputconditioner.v"
 
 module testConditioner();
 
@@ -9,21 +10,38 @@ module testConditioner();
     wire conditioned;
     wire rising;
     wire falling;
-    
+
     inputconditioner dut(.clk(clk),
     			 .noisysignal(pin),
 			 .conditioned(conditioned),
 			 .positiveedge(rising),
-			 .negativeedge(falling))
+			 .negativeedge(falling));
 
 
     // Generate clock (50MHz)
     initial clk=0;
     always #10 clk=!clk;    // 50MHz Clock
-    
+
     initial begin
     // Your Test Code
     // Be sure to test each of the three conditioner functions:
     // Synchronization, Debouncing, Edge Detection
-    
+
+    $dumpfile("inputconditions.vcd");
+    $dumpvars();
+
+    $display("----------------------------------------------------------------------------------");
+    $display("Testing Synchronization");
+    pin =0; #100 pin = 1; #100
+    $display("Pin  CLK   | Ris Fall  Condit  |  Expected outcome");
+    $display("%b  %b  | %b  %b  %b  | %b  %b  %b ", pin, clk,  rising, falling, conditioned, 0,0,0);
+    pin =0; #200
+    $display("%b  %b  | %b  %b  %b  | %b  %b  %b ", pin, clk,  rising, falling, conditioned, 0,0,0);
+
+    $display("Testing Debouncing");
+
+    $display("Testing Edge Detection");
+    $dumpoff;
+    end
+
 endmodule
