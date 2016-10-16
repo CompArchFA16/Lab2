ebe6512ec2bc49b33c6a233d92272e0aae0470bd
apurvaraman <apurva.raman@students.olin.edu>
2016-10-16 17:13:57 -0400

    Input conditioner.

--- inputconditioner.t.v
+++ inputconditioner.t.v
@@ -1,7 +1,7 @@
 //------------------------------------------------------------------------
 // Input Conditioner test bench
 //------------------------------------------------------------------------
-
+`include "inputconditioner.v"
 module testConditioner();
 
     reg clk;
@@ -9,21 +9,41 @@ module testConditioner();
     wire conditioned;
     wire rising;
     wire falling;
-    
+    integer i;
     inputconditioner dut(.clk(clk),
     			 .noisysignal(pin),
 			 .conditioned(conditioned),
 			 .positiveedge(rising),
-			 .negativeedge(falling))
+			 .negativeedge(falling));
 
 
     // Generate clock (50MHz)
-    initial clk=0;
+    initial clk=1;
     always #10 clk=!clk;    // 50MHz Clock
-    
+
     initial begin
+
+    $dumpfile("inputconditioner.vcd");
+    $dumpvars(0, dut);
     // Your Test Code
     // Be sure to test each of the three conditioner functions:
-    // Synchronization, Debouncing, Edge Detection
-    
+    // Synchronize, Clean, Preprocess (edge finding)
+    $display("Clock Pin | Conditioned Rising Falling");
+    //check a noisy signal
+    //1 clock cycle
+    pin = 0;  #5; pin = 1; #5; pin = 0;  #5; pin = 1; #5;
+    $display("%b   %b | %b   %b   %b", clk, pin, conditioned, rising, falling);
+    $display($time);
+    //off then on
+    //4 clock cycles
+    pin = 0;  #40; pin = 1; #40;
+    $display("%b   %b | %b   %b   %b", clk, pin, conditioned, rising, falling);
+    $display($time);
+    //on then off
+    //4 clock cycles
+    pin = 1;  #40; pin = 0; #40;
+    $display("%b   %b | %b   %b   %b", clk, pin, conditioned, rising, falling);
+    $display($time);
+end
+
 endmodule
--- inputconditioner.v
+++ inputconditioner.v
@@ -16,23 +16,46 @@ output reg  negativeedge    // 1 clk pulse at falling edge of conditioned
 
     parameter counterwidth = 3; // Counter size, in bits, >= log2(waittime)
     parameter waittime = 3;     // Debounce delay, in clock cycles
-    
+
     reg[counterwidth-1:0] counter = 0;
     reg synchronizer0 = 0;
     reg synchronizer1 = 0;
-    
+
+    initial begin //set start values
+      conditioned <= 0;
+      positiveedge <= 0;
+      negativeedge <= 0;
+    end
+
     always @(posedge clk ) begin
-        if(conditioned == synchronizer1)
-            counter <= 0;
-        else begin
-            if( counter == waittime) begin
-                counter <= 0;
-                conditioned <= synchronizer1;
-            end
-            else 
-                counter <= counter+1;
+     	positiveedge <= 0; //reset at the beginning when the positive edge of the clock is triggered
+     	negativeedge <= 0;
+
+      if(conditioned == synchronizer1)begin
+        counter <= 0;
+     	end
+
+      else begin
+        if(counter == waittime)begin
+          counter <= 0;
+          conditioned <= synchronizer1;
+     		  if (synchronizer1) begin //simpler than checking if one is greater than the other
+            positiveedge <= 1;
+            //negativeedge <= 0; //default is 0
+            //can't do set the other value only here because it will be done simultaneously(messes up waveform)
+            //it needs to reset every clock cycle and can't change during that cycle
+          end
+          else begin
+     			  negativeedge <= 1;
+            //positiveedge <= 0;
+     		  end
+
         end
-        synchronizer0 <= noisysignal;
-        synchronizer1 <= synchronizer0;
+
+        else
+          counter <= counter + 1;
+     end
+     synchronizer0 <= noisysignal; //simultaneous because nonblocking, so debounces for 3 clock cycle period
+     synchronizer1 <= synchronizer0;
     end
 endmodule
