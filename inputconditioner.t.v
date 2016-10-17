b7eb7d80477c4e53642bf284d7eaef29e11cfe6b
David Zhu <hdavidzhu@gmail.com>
2016-10-17 11:43:56 -0400

    ADD: Test cases for rising and falling edges.

--- inputconditioner.t.v
+++ inputconditioner.t.v
@@ -29,8 +29,10 @@ module testConditioner();
 
     reg[0:69] noisy;
     reg[0:69] expected;
+    reg[0:69] expected_positiveedge;
+    reg[0:69] expected_negativeedge;
     reg expectedWire;
-    reg synchFailed;
+    reg dutPassed;
 
     integer i;
     initial begin
@@ -38,25 +40,41 @@ module testConditioner();
         $dumpfile("inputconditioner.vcd");
         $dumpvars;
 
-        synchFailed = 0;
+        dutPassed = 1;
         // clock = 70'b0101010101010101010101010101010101010101010101010101010101010101010101;
         noisy    = 70'b0000000000111111111111111111110000000000000000001011111111111111111111;
         expected = 70'bxxxxxxx000000000000001111111111111111111100000000000000000011111111111;
-
+        expected_positiveedge = 70'bxxxxxxx000000000000001100000000000000000000000000000000000011000000000;
+        expected_negativeedge = 70'bxxxxxxxxx0000000000000000000000000000000011000000000000000000000000000;
 
         for (i = 0; i < 70; i=i+1) begin
           pin <= noisy[i];
           expectedWire <= expected[i];
           #10;
+
           if (conditioned !== expected[i]) begin
-            synchFailed = 1;
-            $display("Broken here: %d", i);
-            $display("Expected: %d", expected[i]);
-            $display("Conditioned: %d", conditioned);
+            dutPassed = 0;
+            $display("Expected debounce broken here: %d", i);
+            $display("Expected debounce:    %d", expected[i]);
+            $display("Conditioned debounce: %d", conditioned);
+          end
+
+          if (rising !== expected_positiveedge[i]) begin
+            dutPassed = 0;
+            $display("Expected positiveedge broken here: %d", i);
+            $display("Expected positiveedge:    %d", expected_positiveedge[i]);
+            $display("Conditioned positiveedge: %d", rising);
+          end
+
+          if (falling !== expected_negativeedge[i]) begin
+            dutPassed = 0;
+            $display("Expected negativeedge broken here: %d", i);
+            $display("Expected negativeedge:    %d", expected_negativeedge[i]);
+            $display("Conditioned negativeedge: %d", falling);
           end
         end
 
-        $display("Synchronization Failed: %b", synchFailed);
+        $display("Did all tests pass? %b", dutPassed);
         $finish;
     end
 
