ada1de644e57eee4b6579e7aa8b9999b22026d8e
Anisha <Anisha>
2016-10-17 22:12:45 -0400

    theoretically correct module

--- shiftregister.v
+++ shiftregister.v
@@ -5,6 +5,7 @@
 //      - serial in, parallel out
 //      - parallel in, serial out
 //------------------------------------------------------------------------
+`include "inputconditioner.v"
 
 module shiftregister
 #(parameter width = 8)
@@ -14,12 +15,30 @@ input               peripheralClkEdge,  // Edge indicator
 input               parallelLoad,       // 1 = Load shift reg with parallelDataIn
 input  [width-1:0]  parallelDataIn,     // Load shift reg in parallel
 input               serialDataIn,       // Load shift reg serially
-output [width-1:0]  parallelDataOut,    // Shift reg data contents
-output              serialDataOut       // Positive edge synchronized
+output reg [width-1:0]  parallelDataOut,    // Shift reg data contents
+output reg             serialDataOut       // Positive edge synchronized
 );
 
-    reg [width-1:0]      shiftregistermem;
+    reg [width-1:0] shiftregistermem;
+    wire conditioned;
+    wire positiveedge;
+    wire negativeedge;
+    inputconditioner inputc(clk, peripheralClkEdge, conditioned, positiveedge, negativeedge);
+
     always @(posedge clk) begin
-        // Your Code Here
+       
+        //the shift register advances one position: serialDataIn is loaded into the LSB (Least Significant Bit), and the rest of the bits shift up by one
+        if (positiveedge) begin
+        	shiftregistermem = {{shiftregistermem[width-2:0]}, {serialDataIn}};
+        end
+        //When parallelLoad is asserted, the shift register will take the value of parallelDataIn.
+        else if (parallelLoad) begin
+        	shiftregistermem = parallelDataIn;
+        end
+        //serialDataOut always presents the Most Significant Bit of the shift register.
+        serialDataOut = shiftregistermem[width-1];
+        //parallelDataOut always presents the entirety of the contents of the shift register.
+        parallelDataOut = shiftregistermem;
+
     end
 endmodule
