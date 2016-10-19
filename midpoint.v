df4e355d4e3eb0208c1455fef0e0cc3bd603ae84
yycho0108 <jchocholate@gmail.com>
2016-10-19 20:51:08 +0000

    updated comments for midpoint.v

--- midpoint.v
+++ midpoint.v
@@ -4,9 +4,9 @@
 module midpoint
 (
 	input clk,
-	input [3:0] sw, // 2 switches, sw[0] for Serial Data In, sw[1] for Clk Ed, sw[2] for "show lower 4 bits" and sw[3] for "show higher 4 bits"
+	input [3:0] sw, // 2 switches, sw[0] for Serial Data In, sw[1] for Clk Ed, sw[2] for "show low 4 bits" and !sw[2] for "show high 4 bits"
 	input [3:0] btn, // btn[0] for parallel load
-	output [3:0] led // 4 leds, display subset of status of shift register
+	output reg [3:0] led // 4 leds, display subset of status of shift register
   );
 
   wire[2:0] conditioned, posiedge, negaedge;
@@ -17,11 +17,14 @@ module midpoint
   inputconditioner ic1(clk, sw[0], conditioned[1], posiedge[1], negaedge[1]);
   inputconditioner ic2(clk, sw[1], conditioned[2], posiedge[2], negaedge[2]);
 
-  shiftregister sr0(clk, posiedge[2], negaedge[0], 8'ha5, conditioned[1], parallelOut, serialOut);
+  shiftregister sr0(clk, posiedge[2], negaedge[0], 8'hA5, conditioned[1], parallelOut, serialOut);
   
-  if (sw[2] == 1)
-    assign led = parallelOut[3:0]
-  else 
-    assign led = parallelOut[7:4]
-
+  always @(posedge clk) begin
+      if (sw[2]) begin
+        led = parallelOut[3:0];
+      end else begin
+        led = parallelOut[7:4];
+      end
+  end
+  
 endmodule
