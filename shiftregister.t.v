2d8f8272295a9392ac0632e0f3cace8597961ca8
Zarin Bhuiyan <zarin.bhuiyan@students.olin.edu>
2016-10-18 20:43:43 -0400

    added first test case to shift register

--- shiftregister.t.v
+++ shiftregister.t.v
@@ -2,6 +2,8 @@
 // Shift Register test bench
 //------------------------------------------------------------------------
 
+`include "shiftregister.v"
+
 module testshiftregister();
 
     reg             clk;
@@ -20,9 +22,34 @@ module testshiftregister();
     		           .serialDataIn(serialDataIn), 
     		           .parallelDataOut(parallelDataOut), 
     		           .serialDataOut(serialDataOut));
-    
+   
+    initial clk=0;
+    always #10 clk=!clk;    // 50MHz Clock
+
+    integer i;
+    reg dutPassed;
+    reg[7:0] data;
     initial begin
-    	// Your Test Code
+        $dumpfile("testshiftregister.vcd");
+        $dumpvars;
+
+        dutPassed = 1; 
+
+        //First Test Case
+        data = 8'b10101010; //arbitrary 
+        parallelLoad = 0; //false
+
+        for (i=0; i < 8; i=i+1) begin
+            serialDataIn <= data[i];
+        end 
+
+        if (parallelDataOut !== data) begin
+            dutPassed = 0;
+            $display("Serial in, parellel out test pass? %b", dutPassed);
+        end
+       
+        $display("Did all tests pass? %b", dutPassed);
+        $finish;
     end
 
 endmodule
