1df42e2f02b01d9494b0aa251fe951a3e1405ce5
Jee Hyun Kim <jeehyun.kim@students.olin.edu>
2016-10-19 16:47:06 -0400

    mid

--- midpoint.v
+++ midpoint.v
@@ -3,11 +3,24 @@
 
 `timescale 1ns / 1ps
 
+// Two-input MUX with parameterized bit width (default: 1-bit)
+module mux_p #(parameter W = 4)
+(
+    input[W-1:0]    in0,
+    input[W-1:0]    in1,
+    input           sel,
+    output[W-1:0]   out
+);
+    // Conditional operator - http://www.verilog.renerta.com/source/vrg00010.htm
+    assign out = (sel) ? in1 : in0;
+endmodule
+
+
 module midpoint
 (
     input        clk,
-    input  [1:0] sw,
-    input  [1:0] btn,
+    input  [3:0] sw,
+    input  [3:0] btn,
     output [3:0] led
 );
 	
@@ -50,6 +63,5 @@ module midpoint
     		           .parallelDataOut(parallelDataOut), 
     		           .serialDataOut(serialDataOut));
 
-
-   	assign led = parallelDataOut[3:0];
+    mux_p #(4) ledout(.in0(parallelDataOut[3:0]), .in1(rparallelDataOut[7:4]), .sel(sw[3]), .out(led));
 endmodule
