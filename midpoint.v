3e33c1e5208440e5f602c4f7016a8c542c18de8d
shrutiyer <shruti.iyer@students.olin.edu>
2016-10-19 19:51:16 +0000

    ADD: led output in midpoint.v

--- midpoint.v
+++ midpoint.v
@@ -4,8 +4,8 @@
 module midpoint
 (
 	input clk,
-	input [3:0] sw, // 2 switches, sw[0] for Serial Data In, sw[1] for Clk Ed
-	input [3:0] btn, // btn[0] for parallel load, btn[1] for "show lower 4 bits" and btn[2] for "show higher 4 bits"
+	input [3:0] sw, // 2 switches, sw[0] for Serial Data In, sw[1] for Clk Ed, sw[2] for "show lower 4 bits" and sw[3] for "show higher 4 bits"
+	input [3:0] btn, // btn[0] for parallel load
 	output [3:0] led // 4 leds, display subset of status of shift register
   );
 
@@ -18,5 +18,10 @@ module midpoint
   inputconditioner ic2(clk, sw[1], conditioned[2], posiedge[2], negaedge[2]);
 
   shiftregister sr0(clk, posiedge[2], negaedge[0], 8'ha5, conditioned[1], parallelOut, serialOut);
+  
+  if (sw[2] == 1)
+    assign led = parallelOut[3:0]
+  else 
+    assign led = parallelOut[7:4]
 
 endmodule
