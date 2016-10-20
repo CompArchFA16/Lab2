766198c72313cb3febc66aa15a53171f6364cc67
Anisha <Anisha>
2016-10-20 00:57:37 -0400

    adding labwrapper

--- labwrapper.v
+++ labwrapper.v
@@ -5,7 +5,7 @@
 //
 //  Usage:
 //     btn0 - button0 input
-//     btn1 - switch which bits are displayed on the LEDs
+//     sw2 - switch which bits are displayed on the LEDs
 //     sw0 - noisy signal --> conditioned --> serial in
 //     sw1 - noisy signal --> pos edge --> peripheral clk
 //
@@ -14,7 +14,7 @@
 //--------------------------------------------------------------------------------
 
 `timescale 1ns / 1ps
-
+`include "midpoint.v"
 
 //--------------------------------------------------------------------------------
 // Basic building block modules
@@ -93,6 +93,11 @@ module lab0_wrapper
     wire res_sel;             // Select between display options
     wire cout;                // Carry out from adder
     wire ovf;                 // Overflow from adder
+
+    wire [7:0] parallelDataIn;
+    assign parallelDataIn = 8'b10101010;
+    wire serialOut;
+    wire[7:0] res;
     
     // Memory for stored operands (parametric width set to 4 bits)
     dff #(4) opA_mem(.trigger(clk), .enable(btn[0]), .d(sw), .q(opA));
@@ -100,10 +105,14 @@ module lab0_wrapper
     
     // Capture button input to switch which MUX input to LEDs
     jkff1 src_sel(.trigger(clk), .j(btn[3]), .k(btn[2]), .q(res_sel));
-    mux2 #(4) output_select(.in0(res0), .in1(res1), .sel(res_sel), .out(led));
+    mux2 #(4) output_select(.in0(res0), .in1(res1), .sel(sw[2]), .out(led));
     
     // TODO: You write this in your adder.v
-    FullAdder4bit adder(.sum(res0), .carryout(cout), .overflow(ovf), .a(opA), .b(opB));
+    //FullAdder4bit adder(.sum(res0), .carryout(cout), .overflow(ovf), .a(opA), .b(opB));
+    midpoint midpt(btn[0], sw[0], sw[1], clk, parallelDataIn, serialOut, res);
+
+    assign res0 = res[3:0];
+    assign res1 = res[7:4];
 
     // Assign bits of second display output to show carry out and overflow
     assign res1[0] = cout;
