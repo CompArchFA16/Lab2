a8c95adcfc2456249380cc2a671ddaa5bd5e6078
Jee Hyun Kim <jeehyun.kim@students.olin.edu>
2016-10-23 12:05:47 -0400

    added midpoint, datamem and fsm

--- spimemory.v
+++ spimemory.v
@@ -1,6 +1,12 @@
 //------------------------------------------------------------------------
 // SPI Memory
+// still needs address latch, DFF, buffer
 //------------------------------------------------------------------------
+
+`include "shiftregister.v"
+`include "inputconditioner.v"
+`include "fsmachine.v"
+`include "datamemory.v"
 
 module spiMemory
 (
@@ -12,6 +18,58 @@ module spiMemory
     output [3:0]    leds        // LEDs for debugging
 )
 
+	wire [7:0] parallelDataIn; //xA5
+	wire [7:0] parallelDataOut;
+	wire SerialDataOut;
+
+    wire conditioned, rising, falling;
+    wire dummy0, dummy1, dummy2, dummy3, dummy4, dummy5;
+
+    wire misoBufe, dmWe, addrWe,  srWe;
+
+    reg [6:0] address; 
+
+    inputconditioner ic0(.clk(clk),
+    			 		 .noisysignal(mosi_pin),
+						 .conditioned(dummy0),
+						 .positiveedge(dummy1),
+						 .negativeedge(falling));
+
+    inputconditioner ic1(.clk(clk),
+    			 		 .noisysignal(sclk_pin),
+						 .conditioned(conditioned),
+						 .positiveedge(dummy2),
+						 .negativeedge(dummy3));
+
+	inputconditioner ic2(.clk(clk),
+    			 		 .noisysignal(cs_pin), 
+						 .conditioned(dummy4),
+						 .positiveedge(rising),
+						 .negativeedge(dummy5));
+    
+    // Instantiate with parameter width = 8
+    shiftregister #(8) sr(.clk(clk), 
+    		           .peripheralClkEdge(rising),
+    		           .parallelLoad(falling), 
+    		           .parallelDataIn(parallelDataIn), 
+    		           .serialDataIn(conditioned), 
+    		           .parallelDataOut(parallelDataOut), 
+    		           .serialDataOut(serialDataOut));
+
+    datamemory dm(.clk(clk)
+    			  .dataOut(parallelDataIn[7:0])
+    			  .address(address[6:0])
+    			  .writeEnable(dmWe)
+    			  .dataIn(parallelDataOut[7:0]));
+
+	fsmachine fsm(.clk(clk),
+				  .sclk(rising),
+				  .cs(conditioned),
+				  .rw(parallelDataOut[0]),
+				  .misobuff(misoBufe),
+				  .dm(dmWe),
+				  .addr(addrWe),
+				  .sr(srWe));
 
 endmodule
    
