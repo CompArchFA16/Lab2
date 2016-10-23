58340454b8efdc23d2cf7be1f2a7e40db45cf095
Ian Hill <ian.hill@students.olin.edu>
2016-10-23 00:02:37 -0400

    complete midpoint.v

--- midpoint.v
+++ midpoint.v
@@ -8,14 +8,15 @@
 module midpoint
 #(parameter width = 8)
 (
+input clk,
 input button0,
 input switch0,
 input switch1,
 output [width-1:0] leds
 );
     reg clk;
+    reg[7:0] const = "xa5";
     wire btn_to_prl_ld, swt_to_srl_in, swt_to_clk_edg;
-    always #10 clk=!clk; // 50MHz Clock
     inputconditioner ic0(
         .clk(clk),
         .noisysignal(button0),
@@ -41,7 +42,7 @@ output [width-1:0] leds
         .clk(clk),
         .peripheralClkEdge(swt_to_clk_edg),
         .parallelLoad(btn_to_prl_ld),
-        .parallelDataIn(),
+        .parallelDataIn(const),
         .serialDataIn(swt_to_srl_in),
         .parallelDataOut(leds),
         .serialDataOut()
