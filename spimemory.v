041f54ecf471c32217c4a318a2067eae853607d0
shrutiyer <shruti.iyer@students.olin.edu>
2016-10-23 17:13:49 +0000

    ADD: spimemory

--- spimemory.v
+++ spimemory.v
@@ -1,6 +1,10 @@
 `include "finitestatemachine.v"
 `include "addresslatch.v"
 `include "shiftregister.v"
+`include "dflipflop.v"
+`include "inputconditioner.v"
+`include "datamemory.v"
+`include "misobuffer.v"
 
 //------------------------------------------------------------------------
 // SPI Memory
@@ -16,13 +20,40 @@ module spiMemory
     output [3:0]    leds        // LEDs for debugging
 )
 
-wire miso_buff, dm_we, addr_we, sr_we;
+  wire miso_buff, dm_we, addr_we, sr_we;
+  wire mosi_conditioned, sclk_conditioned, cs_conditioned;
+  wire mosi_posedge, sclk_posedge, cs_posedge;
+  wire mosi_negedge, sclk_negedge, cs_negedge;
+  wire [7:0] parallelDataIn, parallelDataOut, addrOut;
+  wire serialDataOut, dffOut;
 
-reg [7:0] addr;
+  reg [7:0] addr;
 
-finitestatemachine fsm(peripheralClkEdge, conditioned, miso_buff, dm_we, addr_we, sr_we);
-shiftregister #(8) sr(clk, peripheralClkEdge, sr_we, parallelDataIn, serialDataIn, parallelDataOut, serialDataOut);
-addresslatch al(clk, addr_we, parallelDataOut, addr);
-datamemory #(7) dm(clk, parallelDataIn, addr, dm_we, parallelDataOut); //may be wrong declaration
+  // mosi inputconditioner
+  inputconditioner mosi(clk, mosi_pin, mosi_conditioned, mosi_posedge, mosi_negedge);
+
+  // sclk inputconditioner
+  inputconditioner sclk(clk, sclk_pin, sclk_conditioned, sclk_posedge, sclk_negedge);
+
+  // cs inputconditioner
+  inputconditioner cs(clk, cs_pin, cs_conditioned, cs_posedge, cs_negedge);
+
+  // Shift Register
+  shiftregister #(8) sr(clk, sclk_posedge, sr_we, parallelDataIn, mosi_conditioned, parallelDataOut, serialDataOut);
+  
+  // Finite State Machine
+  finitestatemachine fsm(sclk_posedge, cs_conditioned, miso_buff, dm_we, addr_we, sr_we);
+
+  // Address Latch
+  addresslatch al(clk, addr_we, parallelDataOut, addrOut);
+
+  // Data memory
+  datamemory dm(clk, parallelDataIn, addrOut[6:0], dm_we, parallelDataOut); //may be wrong declaration
+
+  // D Flip Flop
+  dff dff(clk, sclk_negedge, serialDataOut, dffOut);
+
+  // miso buffer
+  misobuffer mb(dffOut, miso_buff, miso_pin);
 
 endmodule
