0571152644bb2d2a7bb3ce396adb41d2036e7869
Bonnie Ishiguro <bgishiguro@gmail.com>
2016-10-23 15:42:12 -0400

    SPI top level - input conditioners, data memory, shift register

--- spimemory.v
+++ spimemory.v
@@ -1,6 +1,9 @@
 //------------------------------------------------------------------------
 // SPI Memory
 //------------------------------------------------------------------------
+`include "inputconditioner.v"
+`include "shiftregister.v"
+`include "datamemory.v"
 
 module spiMemory
 (
@@ -10,8 +13,32 @@ module spiMemory
     output          miso_pin,   // SPI master in slave out
     input           mosi_pin,   // SPI master out slave in
     output [3:0]    leds        // LEDs for debugging
-)
+)	
+	// input conditioner wires
+	wire conditionedMosi;
+	wire conditionedCS;
+	wire positiveedge;
+	wire negativeedge;
 
+	// shift register wires
+	wire parallelOut;
+	wire serialOut;
+
+	// data memory wires
+	wire[7:0] DMDataOut;
+	wire[7:0] DMDataIn;
+	wire DMWriteEnable;
+	wire[6:0] DMAddress;
+
+	inputconditioner conditioner1(.conditioned(conditioned), .clk(clk), .noisysignal(mosi_pin));
+    inputconditioner conditioner2(.positiveedge(positiveedge), .negativeedge(negativeedge), .clk(clk), .noisysignal(sclk_pin));
+    inputconditioner conditioner3(.conditioned(conditionedCS), .clk(clk), .noisysignal(cs_pin));
+    
+    datamemory mem(.dataOut(DMDataOut), .clk(clk), .address(DMAddress), .writeEnable(DMWriteEnable), .dataIn(DMDataIn));
+
+    shiftregister register(.parallelDataOut(parallelOut), .serialDataOut(serialOut), 
+    	.serialDataIn(conditionedMosi), .peripheralClkEdge(positiveedge), 
+    	.parallelDataIn(DMDataOut), .clk(clk));
 
 endmodule
    
