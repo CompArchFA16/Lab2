ebcea3c5e9ca76d97d05e65d436bd847dd280872
Jee Hyun Kim <jeehyun.kim@students.olin.edu>
2016-10-23 10:15:56 -0400

    added comments

--- fsmachine.v
+++ fsmachine.v
@@ -1,13 +1,13 @@
 module fsmachine
 (	
 	input clk,
-	input sclk,
-	input cs, 
-	input rw, 
-	output reg misobuff,
-	output reg dm,
-	output reg addr,
-	output reg sr
+	input sclk, //clk edge
+	input cs, //conditioned
+	input rw, //shift register output[0]
+	output reg misobuff, //MISO enable
+	output reg dm, //data memory write enable
+	output reg addr, //address latch enable
+	output reg sr //shift register write enable
 );
 
 reg state;
@@ -39,32 +39,32 @@ always @(posedge clk) begin
 					count <= count + 1;
 				end
 				else if (sclk) begin
-					state <= 1; //proceed to Got state
+					state <= 1; //proceed to Got
 				end
 			end
 
 			1: begin //Got
 				if (rw) begin
-					state <= 2;
+					state <= 2; //proceed to Read
 				end
 				else begin
-					state <= 5;
+					state <= 5; //proceed to Write
 				end
 			end
 
 			2: begin //Read
-				state <= 3;
+				state <= 3; //proceed to Read2
 			end
 
 			3: begin //Read2
 				sr <= 1;
-				state <= 4;
+				state <= 4; //proceed to Read3
 			end
 
 			4: begin //Read3
 				misobuff <= 1;
 				if (cs == 8) begin
-					state <= 7;
+					state <= 7; //proceed to Done
 				end
 				else if (sclk) begin
 					count <= count + 1;
@@ -73,7 +73,7 @@ always @(posedge clk) begin
 
 			5: begin //Write
 				if (cs == 8) begin
-					state <= 6;
+					state <= 6; //proceed to Write2
 				end
 				else if (sclk) begin
 					count <= count + 1;
@@ -81,7 +81,7 @@ always @(posedge clk) begin
 			end
 
 			6: begin //Write2
-				state <= 7;
+				state <= 7; //proceed to Done
 				dm <= 1;
 			end
 
