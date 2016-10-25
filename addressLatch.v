9281b312a7cf7588a0bc1c0b6ab5c84cba2a0c71
lisahachmann <lisa.hachmann@gmail.com>
2016-10-25 19:15:30 -0400

    Changed address latch input size

--- addressLatch.v
+++ addressLatch.v
@@ -6,9 +6,9 @@
 module addressLatch
 (
     input           clk,  
-    input [7:0]     d,   
+    input [6:0]     d,   
     input           ce,     
-    output reg [7:0]    q   
+    output reg [6:0]    q   
 );
 
 	always @(posedge clk) begin
