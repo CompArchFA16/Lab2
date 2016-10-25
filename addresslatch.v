beea88ff8c235a5d3e06a91368e96271abbee064
David Zhu <hdavidzhu@gmail.com>
2016-10-24 20:09:56 -0400

    ADD: Basic FSM structure.

--- addresslatch.v
+++ addresslatch.v
@@ -1,15 +1,13 @@
 module addressLatch
 (
-	output[7:0] addressLatchOut;
-	input clk;
-	input writeEnable;
-	input[7:0] addressLatchIn;
-)
-
+	output reg [7:0] addressLatchOut,
+	input clk,
+	input writeEnable,
+	input[7:0] addressLatchIn
+);
 	always @(posedge clk) begin
 		if (writeEnable == 1) begin
 			addressLatchOut <= addressLatchIn;
 		end
 	end
-
 endmodule
--- fsm.v
+++ fsm.v
@@ -1,12 +1,30 @@
 module fsm
 (
-	output misoBufe;
-	output DMWriteEnable;
-	output addressWriteEnable;
-	output SRWriteEnable;
-	output peripheralClkEdge;
-	input shiftRegOutP;
-	input conditioned;
+	output misoBufferEnable,
+	output DMWriteEnable,
+	output addressWriteEnable,
+	output SRWriteEnable,
+
+  input  sClkPosEdge,
+	input  readWriteEnable, // This tells us read/write
+	input  chipSelectConditioned
 );
 
+  parameter waitTime = 8;
+  reg [2:0] counter = 0;
+
+  always @ (negedge sClkPosEdge) begin
+    if (chipSelectConditioned === 1'b0) begin
+      if (counter === waitTime) begin
+        counter <= 0;
+      end
+      else begin
+      end
+      counter <= counter + 1;
+    end
+    else begin
+      // It will reset everything.
+      counter <= 0;
+    end
+  end
 endmodule
