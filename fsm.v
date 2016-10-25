5a535a7e04b99e9dfc15868573febb1827fbcf91
David Zhu <hdavidzhu@gmail.com>
2016-10-24 20:18:30 -0400

    ADD: More wiring to FSM.

--- fsm.v
+++ fsm.v
@@ -1,9 +1,9 @@
 module fsm
 (
-	output misoBufferEnable,
-	output DMWriteEnable,
-	output addressWriteEnable,
-	output SRWriteEnable,
+	output reg misoBufferEnable,
+	output reg DMWriteEnable,
+	output reg addressWriteEnable,
+	output reg SRWriteEnable,
 
   input  sClkPosEdge,
 	input  readWriteEnable, // This tells us read/write
@@ -14,17 +14,33 @@ module fsm
   reg [2:0] counter = 0;
 
   always @ (negedge sClkPosEdge) begin
+
     if (chipSelectConditioned === 1'b0) begin
+
       if (counter === waitTime) begin
         counter <= 0;
+        addressWriteEnable <= 1;
+
+        if (readWriteEnable === 1) begin
+          // Here, we set read signals to high.
+          SRWriteEnable <= 1;
+          misoBufferEnable <= 1;
+        end
+        else begin
+          // Here we set circuit into write mode.
+          DMWriteEnable <= 1;
+        end
       end
-      else begin
-      end
+
       counter <= counter + 1;
     end
     else begin
       // It will reset everything.
       counter <= 0;
+      misoBufferEnable <= 0;
+      DMWriteEnable <= 0;
+      addressWriteEnable <= 0;
+      SRWriteEnable <= 0;
     end
   end
 endmodule
