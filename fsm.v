a37634bc4d261f80ca1cab7ec63d41d2a28c7e0f
byronwasti <byron.wasti@gmail.com>
2016-10-25 22:03:06 -0400

    Fixing verilog bugs!

--- fsm.v
+++ fsm.v
@@ -1,38 +1,38 @@
 // Finite State Machine for logic control
 
-`define GET 'd0;
-`define GOT 'd1;
-`define READ1 'd2;
-`define READ2 'd3;
-`define READ3 'd4;
-`define WRITE1 'd5;
-`define WRITE2 'd6;
-`define DONE 'd7;
+`define GET 3'd0
+`define GOT 3'd1
+`define READ1 3'd2
+`define READ2 3'd3
+`define READ3 3'd4
+`define WRITE1 3'd5
+`define WRITE2 3'd6
+`define DONE 3'd7
 
 module fsm 
 (
     input clkedge,
     input cs,
     input lsbsrop,
-    output miso_bufe,
-    output dm_we,
-    output addre_we,
-    output sr_we
-)
+    output reg miso_bufe,
+    output reg dm_we,
+    output reg addre_we,
+    output reg sr_we
+);
     reg [2:0] count, state;
-    count <= 0;
-    state <= `GET;
+    //count <= 0;
+    //state <= `GET;
 
     always @(posedge clkedge) begin
-        case (state) begin
-          `GET:     begin
+        case (state)
+          `GET :    begin
                         count <= count + 1;
-                        if (count == 8) begin
+                        if (count == 3'd8) begin
                             count <= 0;
                             state <= `GOT;
                         end
                     end
-          `GOT:     begin
+          `GOT :     begin
                         if (lsbsrop == 1) begin
                             state <= `READ1;
                         end
@@ -53,14 +53,14 @@ module fsm
           `READ3:   begin
                         count <= count + 1;
                         miso_bufe <= 1;
-                        if (count == 8) begin
+                        if (count == 3'd8) begin
                             state <= `DONE;
                             count <= 0;
                         end
                     end
           `WRITE1:  begin
                         count <= count + 1;
-                        if (count == 8) begin
+                        if (count == 3'd8) begin
                             state <= `WRITE2;
                             count <= 0;
                         end
@@ -70,16 +70,16 @@ module fsm
                         state <= `DONE;
                     end
           `DONE:    begin
-                        counter <= 0;
+                        count <= 0;
                         sr_we <= 0;
                         dm_we <= 0;
                         addre_we <= 0;
                         miso_bufe <= 0;
                         if (cs == 1) begin
-                            state <= `GET
+                            state <= `GET;
                         end
                     end
-          default: 
+            default: begin end
         endcase
     end
 
--- spimemory.v
+++ spimemory.v
@@ -1,3 +1,8 @@
+`include "inputconditioner.v"
+`include "shiftregister.v"
+`include "fsm.v"
+`include "datamemory.v"
+
 //------------------------------------------------------------------------
 // SPI Memory
 //------------------------------------------------------------------------
@@ -7,8 +12,8 @@ module dff
     input d,
     input ce,
     input clk,
-    output q
-)
+    output reg q
+);
     always @(posedge clk) begin
         if (ce == 1) begin
             q <= d;
@@ -21,8 +26,8 @@ module dff8bit
     input [7:0] d,
     input ce,
     input clk,
-    output [7:0] q
-)
+    output reg [7:0] q
+);
     always @(posedge clk) begin
         if (ce == 1) begin
             q <= d;
@@ -35,9 +40,15 @@ module bufferSwitch
     input in,
     input enable,
     output out
-)
-    wire out
+);
+    wire out;
     assign out = (enable) ? in : 1'bz;
+   //out <= (enable) ? in : 1'bz;
+   /*
+   always @(enable) begin
+       out <= 1'bz;
+   end
+   */
 
 endmodule
 
@@ -49,12 +60,12 @@ module spiMemory
     output          miso_pin,   // SPI master in slave out
     input           mosi_pin,   // SPI master out slave in
     output [3:0]    leds        // LEDs for debugging
-)
+);
     
     wire [2:0] rising, falling, conditioned;
     wire [7:0] parallelOut, parallelIn;
     wire serialOut, parallelLoad;
-    wire [6:0] address;
+    wire [7:0] address;
     wire addre_we, dm_we, miso_bufe, sr_we;
 
 	inputconditioner mosiIC(clk, mosi_pin, conditioned[0], rising[0], falling[0]);
@@ -66,13 +77,14 @@ module spiMemory
     // DFF & MISO_BUFE
     wire miso_pin_pre_buffer;
     dff dff0(serialOut, falling[1], clk, miso_pin_pre_buffer);
-    bufferSwitch(miso_pin_pre_buffer, miso_bufe, miso_pin);
+    bufferSwitch buffswitch0(miso_pin_pre_buffer, miso_bufe, miso_pin);
 
     // Memory
     datamemory memory0(clk, parallelIn, address, dm_we, parallelOut);
     dff8bit dff1(parallelOut, addre_we, clk, address);
 
     // FSM
+    fsm fsm0(rising[1], conditioned[2], parallelOut[0], miso_bufe, dm_we, addre_we, sr_we);
 
 endmodule
    
