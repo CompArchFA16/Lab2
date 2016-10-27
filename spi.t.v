dfe95092860f39cc43db140979773323b5d177ca
abuchele <anna.buchele@students.olin.edu>
2016-10-26 20:51:05 -0400

    added translation from human to serial

--- spi.t.v
+++ spi.t.v
@@ -19,20 +19,31 @@ module testspi();
     		           .miso_pin(miso_pin), 
     		           .mosi_pin(mosi_pin), 
     		           .leds(leds));
-    
+     reg [7:0] mosi_h; //8 bit mosi signal that is human readable
 
     initial begin
-        clk = 0;
-        sclk_pin = 0;
+
+        mosi_h <= 8'b_0010_1010;
+        clk <= 0;
+        sclk_pin <= 0;
+        cs_pin <= 0;
     end
 
     always begin
         #10 clk = ~clk;
-        #10 sclk_pin = ~sclk_pin;
+            sclk_pin = ~sclk_pin;
     end
 
     initial begin
-
+        #30 cs_pin = 1;
+        #10 mosi_pin = mosi_h[7];
+        #10 mosi_pin = mosi_h[6];
+        #10 mosi_pin = mosi_h[5];
+        #10 mosi_pin = mosi_h[4];
+        #10 mosi_pin = mosi_h[3];
+        #10 mosi_pin = mosi_h[2];
+        #10 mosi_pin = mosi_h[1];
+        #10 mosi_pin = mosi_h[0];
 
         $finish;
     end
