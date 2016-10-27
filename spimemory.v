29bd331e2e781ff176497d65d0b8e806164488bc
David Zhu <hdavidzhu@gmail.com>
2016-10-26 22:42:32 -0400

    CONNECT: SPI.

--- spimemory.v
+++ spimemory.v
@@ -6,6 +6,7 @@
 `include "datamemory.v"
 `include "addresslatch.v"
 `include "fsm.v"
+`include "misoSoup.v"
 
 module spiMemory
 (
@@ -15,7 +16,7 @@ module spiMemory
     output          miso_pin,   // SPI master in slave out
     input           mosi_pin,   // SPI master out slave in
     output [3:0]    leds        // LEDs for debugging
-)	
+);
 	// input conditioner wires
 	wire conditionedMosi;
 	wire conditionedCS;
@@ -23,7 +24,7 @@ module spiMemory
 	wire negativeedge;
 
 	// shift register wires
-	wire parallelOut;
+	wire[7:0] parallelOut;
 	wire serialOut;
 
 	// data memory wires
@@ -37,34 +38,34 @@ module spiMemory
 	wire addressWriteEnable;
 	wire misoBufferEnable;
 
-	inputconditioner conditioner1(.conditioned(conditioned), 
-								  .clk(clk), 
+	inputconditioner conditioner1(.conditioned(conditionedMosi),
+								  .clk(clk),
 								  .noisysignal(mosi_pin)
 	);
 
-    inputconditioner conditioner2(.positiveedge(positiveedge), 
-    							  .negativeedge(negativeedge), 
-    							  .clk(clk), 
+    inputconditioner conditioner2(.positiveedge(positiveedge),
+    							  .negativeedge(negativeedge),
+    							  .clk(clk),
     							  .noisysignal(sclk_pin)
 
     );
     inputconditioner conditioner3(.conditioned(conditionedCS),
-    							  .clk(clk), 
+    							  .clk(clk),
     							  .noisysignal(cs_pin)
     );
-    
-    datamemory mem(.dataOut(DMDataOut), 
-    			   .clk(clk), 
-    			   .address(DMAddress), 
-    			   .writeEnable(DMWriteEnable), 
+
+    datamemory mem(.dataOut(DMDataOut),
+    			   .clk(clk),
+    			   .address(DMAddress),
+    			   .writeEnable(DMWriteEnable),
     			   .dataIn(DMDataIn)
     );
 
-    shiftregister register(.parallelDataOut(parallelOut), 
-    					   .serialDataOut(serialOut), 
-    					   .serialDataIn(conditionedMosi), 
-    					   .peripheralClkEdge(positiveedge), 
-    					   .parallelDataIn(DMDataOut), 
+    shiftregister register(.parallelDataOut(parallelOut),
+    					   .serialDataOut(serialOut),
+    					   .serialDataIn(conditionedMosi),
+    					   .peripheralClkEdge(positiveedge),
+    					   .parallelDataIn(DMDataOut),
     					   .clk(clk),
     					   .parallelLoad(SRWriteEnable)
     );
@@ -73,10 +74,20 @@ module spiMemory
     		.DMWriteEnable(DMWriteEnable),
     		.addressWriteEnable(addressWriteEnable),
     		.SRWriteEnable(SRWriteEnable),
+        .clk(clk),
     		.sClkPosEdge(positiveedge),
     		.readWriteEnable(parallelOut[0]),
     		.chipSelectConditioned(conditionedCS)
     );
 
+    addressLatch latch(.addressLatchOut(DMAddress),
+                       .clk(clk),
+                       .writeEnable(addressWriteEnable),
+                       .addressLatchIn(parallelOut[7:1]) );
+
+    misoSoup misoOut(.q(miso_pin),
+            .d(serialOut),
+            .writeEnable(negativeedge),
+            .misoBufe(misoBufferEnable),
+            .clk(clk));
 endmodule
-   
