068990a42a0578df1a825218223e1f0bc7e63eeb
lisahachmann <lisa.hachmann@gmail.com>
2016-10-26 22:13:50 -0400

    Added quick test modules

--- addressLatch.v
+++ addressLatch.v
@@ -36,44 +36,43 @@ module dFlipFlop
 
 endmodule
 
-module quicktest();
-    //variables for testing address latch
-    reg clk;
-    reg[6:0] d;
-    reg ce;
-    wire[6:0] q;
-
-    addressLatch addrTest (clk, d, ce, q);
-
-    initial begin
-        clk = 0; #10
-
-        d = 7'd6;
-        ce = 1;
-        clk = 1; #10;
-
-        $display("Output address latch: %d", q);
-    end
-endmodule
-
-module quicktestDFF ();
-
-//variables for testing d flip flop
-reg clkd;
-reg dd;
-reg ced;
-wire qd;
-
-dFlipFlop dff(clkd, dd, ced, qd);
-
-initial begin
-    clkd = 0; #10
-
-    dd = 1;
-    ced = 1;
-    clkd = 1; #10;
-
-    $display("Output dff: %d", qd);
-end
-
-endmodule
+// module quicktest();
+//     //variables for testing address latch
+//     reg clk;
+//     reg[6:0] d;
+//     reg ce;
+//     wire[6:0] q;
+//
+//     addressLatch addrTest (clk, d, ce, q);
+//
+//     initial begin
+//         clk = 0; #10
+//
+//         d = 7'd6;
+//         ce = 1;
+//         clk = 1; #10;
+//
+//         $display("Output address latch: %d", q);
+//     end
+// endmodule
+//
+// module quicktestDFF ();
+//
+// //variables for testing d flip flop
+// reg clkd;
+// reg dd;
+// reg ced;
+// wire qd;
+//
+// dFlipFlop dff(clkd, dd, ced, qd);
+//
+// initial begin
+//     clkd = 0; #10
+//
+//     dd = 1;
+//     ced = 1;
+//     clkd = 1; #10;
+//
+//     $display("Output dff: %d", qd);
+// end
+// endmodule
