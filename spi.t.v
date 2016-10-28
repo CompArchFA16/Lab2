46b9a9198e609c6e2e07827e99cdc70ddf350e04
apurvaraman <apurva.raman@students.olin.edu>
2016-10-28 16:44:25 -0400

    Adding the test bench.

--- spi.t.v
+++ spi.t.v
@@ -13,11 +13,11 @@ module testspi();
     reg             mosi_pin;
     wire[3:0]       leds;
 
-    spiMemory #(8) dut(.clk(clk), 
+    spiMemory #(8) dut(.clk(clk),
     		           .sclk_pin(sclk_pin),
-    		           .cs_pin(cs_pin), 
-    		           .miso_pin(miso_pin), 
-    		           .mosi_pin(mosi_pin), 
+    		           .cs_pin(cs_pin),
+    		           .miso_pin(miso_pin),
+    		           .mosi_pin(mosi_pin),
     		           .leds(leds));
      reg [7:0] mosi_h; //8 bit mosi signal that is human readable
      reg [3:0] i;
@@ -36,7 +36,7 @@ module testspi();
         #5   clk = ~clk;
     end
     always begin
-        #5    sclk_pin = ~sclk_pin;
+        #50    sclk_pin = ~sclk_pin;
     end
 
     initial begin
@@ -48,10 +48,20 @@ module testspi();
            #100;
         end
             mosi_pin = 1; //8th bit decides read or write, 1 = read, 0 = write
-        #2000 cs_pin = 1;
-    #3000
+
+        //#100 cs_pin = 1;
+        #100  mosi_pin = 0;
+
+    for (i = 7; i > 0; i = i - 1) // Turns mosi_h into serial signal
+        begin
+          mosi_pin <= mosi_h[i];
+          #300;
+        end
+
+        mosi_pin = 0;
+        #10;
+
         $finish;
     end
 
 endmodule
-
